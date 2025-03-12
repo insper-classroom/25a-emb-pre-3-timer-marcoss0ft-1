6585d0a6eb6d3bca5cea2772d2b6753033523555
marcoss0ft <marcosarpc@al.insper.edu.br>
2025-03-12 09:32:16 -0300

    feito

--- exe2/main.c
+++ exe2/main.c
@@ -11,8 +11,24 @@ const int LED_PIN_G = 6;
 volatile int flag_r = 0;
 volatile int flag_g = 0;
 
+volatile int timer0_active = 0;
+volatile int timer1_active = 0;
+
+repeating_timer_t timer_0;
+repeating_timer_t timer_1;
+
+bool timer_0_callback(repeating_timer_t *rt) {
+    gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R));
+    return true; // Mantém o timer repetindo
+}
+
+bool timer_1_callback(repeating_timer_t *rt) {
+    gpio_put(LED_PIN_G, !gpio_get(LED_PIN_G));
+    return true; // Mantém o timer repetindo
+}
+
 void btn_callback(uint gpio, uint32_t events) {
-    if (events == 0x4) {
+    if (events == GPIO_IRQ_EDGE_FALL) { // Detecta a borda de descida
         if (gpio == BTN_PIN_R)
             flag_r = 1;
         else if (gpio == BTN_PIN_G)
@@ -21,11 +37,15 @@ void btn_callback(uint gpio, uint32_t events) {
 }
 
 int main() {
+    stdio_init_all();
+
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
+    gpio_put(LED_PIN_R, 0); // LED começa apagado
 
     gpio_init(LED_PIN_G);
     gpio_set_dir(LED_PIN_G, GPIO_OUT);
+    gpio_put(LED_PIN_G, 0); // LED começa apagado
 
     gpio_init(BTN_PIN_R);
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
@@ -35,18 +55,35 @@ int main() {
     gpio_set_dir(BTN_PIN_G, GPIO_IN);
     gpio_pull_up(BTN_PIN_G);
 
-    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
-                                       &btn_callback);
+    // Configura interrupções para os dois botões
+    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
     gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
 
     while (true) {
-
         if (flag_r) {
             flag_r = 0;
+            if (!timer0_active) {
+                if (add_repeating_timer_ms(500, timer_0_callback, NULL, &timer_0)) {
+                    timer0_active = 1;
+                }
+            } else {
+                cancel_repeating_timer(&timer_0);
+                timer0_active = 0;
+                gpio_put(LED_PIN_R, 0); // Garante que o LED termine apagado
+            }
         }
 
         if (flag_g) {
             flag_g = 0;
+            if (!timer1_active) {
+                if (add_repeating_timer_ms(250, timer_1_callback, NULL, &timer_1)) {
+                    timer1_active = 1;
+                }
+            } else {
+                cancel_repeating_timer(&timer_1); // Corrigido para cancelar o timer certo
+                timer1_active = 0;
+                gpio_put(LED_PIN_G, 0); // Garante que o LED termine apagado
+            }
         }
     }
 }
--- exe3/main.c
+++ exe3/main.c
@@ -1,20 +1,27 @@
 #include "hardware/gpio.h"
 #include "pico/stdlib.h"
+#include "pico/time.h"
 #include <stdio.h>
 
 const int BTN_PIN_R = 28;
 const int LED_PIN_R = 4;
 
-volatile int flag_f_r = 0;
+volatile int flag_press = 0;
+volatile int flag_release = 0;
+volatile int long_press_detected = 0;
+
+int64_t alarm_callback(alarm_id_t id, void *user_data) {
+    if (gpio_get(BTN_PIN_R) == 0) { // Se o botão ainda estiver pressionado
+        long_press_detected = 1; // Marca que o botão foi pressionado por tempo suficiente
+    }
+    return 0;
+}
 
 void btn_callback(uint gpio, uint32_t events) {
-    if (events == 0x4) { // fall edge
-        if (gpio == BTN_PIN_R)
-            flag_f_r = 1;
-
-    } else if (events == 0x8) { // rise edge
-        if (gpio == BTN_PIN_R)
-            flag_f_r = 0;
+    if (events == GPIO_IRQ_EDGE_FALL) { // Botão pressionado
+        flag_press = 1;
+    } else if (events == GPIO_IRQ_EDGE_RISE) { // Botão solto
+        flag_release = 1;
     }
 }
 
@@ -23,6 +30,7 @@ int main() {
 
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
+    gpio_put(LED_PIN_R, 0); // LED começa apagado
 
     gpio_init(BTN_PIN_R);
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
@@ -32,8 +40,17 @@ int main() {
         BTN_PIN_R, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &btn_callback);
 
     while (true) {
+        if (flag_press) {
+            flag_press = 0;
+            long_press_detected = 0;
+            add_alarm_in_ms(500, alarm_callback, NULL, false); // Inicia alarme para 500ms
+        }
 
-        if (flag_f_r) {
+        if (flag_release) {
+            flag_release = 0;
+            if (long_press_detected) {
+                gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R)); // Alterna o LED
+            }
         }
     }
 }
